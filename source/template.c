d310418c44e9369495d5e95156227cf7b5c6884a
Andreas Luxenburger <luxenburger.andreas@gmail.com>
2015-01-27 15:05:31 +0100

    Fixed displaying issues related to the internal structure of the framebuffer.

--- source/template.c
+++ source/template.c
@@ -11,14 +11,16 @@
 #include "sound_pcm.h"
 #include "bg_music_ogg.h"
 
-#define NUM_PARTICLES 2
+#define NUM_PARTICLES 10
 #define PARTICLE_MAX_WIDTH 20
 #define PARTICLE_MAX_HEIGHT 20
 #define PARTICLE_MIN_WIDTH 2
 #define PARTICLE_MIN_HEIGHT 2
-#define PARTICLE_MAX_SPEED 7
+#define PARTICLE_MAX_SPEED 10
 #define PARTICLE_MIN_SPEED 1
 
+const int PARTICLE_MAX_FREQ  = VOICE_FREQ48KHZ;
+const int PARTICLE_MIN_FREQ = VOICE_FREQ48KHZ>>1;
 const int PARTICLE_MAX_SIZE = PARTICLE_MAX_WIDTH*PARTICLE_MAX_HEIGHT;
 const int PARTICLE_MIN_SIZE = PARTICLE_MIN_WIDTH*PARTICLE_MIN_HEIGHT;
 
@@ -33,81 +35,180 @@ typedef struct {
 	int pos_x, pos_y;   // screen coordinates
 	int size_x, size_y; // dimensions
 	int dx, dy;        // speed values
+	int freq;		   // freequency of collision sound
 }Particle;
 
 Particle g_particles[NUM_PARTICLES];
 
-// PROTOTYPES
-void init();
-void DrawHLine(int, int, int, int);
-void DrawVLine(int, int, int, int);
-void DrawBox (int, int, int, int, int);
-void DrawParticle(int x, int y, int width, int height, int color);
-void initParticles();
-void updateParticles();
-void printInfo();
-
-/******************************************************************************
- * Main method
+/*****************************************************************************
+ * Structure of the external framebuffer xfb[]:                              *
+ *                                                                           *
+ *		0			320			640                                          *
+ *    0 -------------------------> x                                         *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 	240	|xxxxxxxxxxxx|(319,239)                                              *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 		|xxxxxxxxxxxx|                                                       *
+ * 	480	|------------|                                                       *
+ * 		y                                                                    *
+ * 		                                                                     *
+ * 		xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(vmode)) yields a 1D data  *
+ * 		structure of size (vmode->fb_width/2) * vmode->xfb_height storing    *
+ * 		u32-valued data in row major order with half resolution in x-dir.    *
+ * 		                                                                     *
+ * 		The linearized index L of a 2D point                                 *
+ * 		p=(x,y) € [0..fb_width-1] x [0..fb_height-1] then reads as follows:  *
+ * 		                                                                     *
+ * 		L(x,y) = ((vmode->fb_width) * y + x)/VI_DISPLAY_PIX_SZ,              *
+ * 		                                                                     *
+ * 		with VI_DISPLAY_PIX_SZ = 2 and width stride (vmode->fb_width/2).     *
+ *                                                                           *
+ * 		Example:                                                             *
+ *                                                                           *
+ * 		vmode->fb_width = 640, vmode->xfb_height = 480,                      *
+ * 		p = (x,y) = (639,479)                                                *
+ * 		=> L(639,479) = 320 * 479 + (639>>1) = 153599.                       *
  *****************************************************************************/
-int main(int argc, char **argv) {
 
-	init();
+void drawHLine0 (int x1, int x2, int y, int color) {
+    int i;
+    y = 320 * y;
+    x1 >>= 1;
+    x2 >>= 1;
+    for (i = x1; i <= x2; i++) {
+        u32 *tmpfb = xfb;
+        tmpfb[y+i] = color;
+    }
+}
 
-	while(1) {
-		// Call WPAD_ScanPads each loop, this reads the latest controller states
-		WPAD_ScanPads();
+void drawHLine (int x1, int x2, int y, int color) {
+    int i;
+    x1 >>= 1;
+    x2 >>= 1;
+    y *= g_fb_width>>1;
+    u32 *tmpfb = xfb;
+    tmpfb += y;
+    for (i = x1; i <= x2; i++) {
+        tmpfb[i] = color;
+    }
+}
 
-		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
-		// this is a "one shot" state which will not fire again until the button
-		// has been released
-		u32 pressed = WPAD_ButtonsDown(WPAD_CHAN_0);
+void drawVLine0 (int x, int y1, int y2, int color) {
+    int i;
+    x >>= 1;
+    for (i = y1; i <= y2; i++) {
+        u32 *tmpfb = xfb;
+        tmpfb[x + ((640 * i) >> 1)] = color;
+    }
+}
 
-		// We return to the launcher application via exit
-		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+void drawVLine (int x, int y1, int y2, int color) {
+    int i;
+    x >>= 1;
+    u32 *tmpfb = xfb;
+    tmpfb += x;
+    for (i = y1; i <= y2; i++) {
+        tmpfb[(g_fb_width>>1)*i] = color;
+    }
+}
 
-		if( pressed & WPAD_BUTTON_A ) {
-			printf("Button A pressed.\n");
-		}
+void drawBox (int x1, int y1, int x2, int y2, int color) {
+    drawHLine (x1, x2, y1, color);
+    drawHLine (x1, x2, y2, color);
+    drawVLine (x1, y1, y2, color);
+    drawVLine (x2, y1, y2, color);
+}
 
-		u32 buttonsHeld = WPAD_ButtonsHeld(WPAD_CHAN_0);
+void drawParticle(int x1, int y1, int x2, int y2, int color) {
+	int i;
+	for(i=y1; i<=y2; i++)
+		drawHLine(x1, x2, i, color);
+}
 
-		if (buttonsHeld & WPAD_BUTTON_A ) {
-			printf("Button A is being held down.\n");
-		}
+int rnd(int a, int b) {
+	return rand() % (b-a+1) + a;
+}
 
-		u16 buttonsUp = WPAD_ButtonsUp(WPAD_CHAN_0);
+void initParticles() {
+	srand(time(NULL));
+	int i, size;
+	for(i=0; i<NUM_PARTICLES; i++) {
+		// Create particle with random size, position and speed
+		g_particles[i].size_x = rnd(PARTICLE_MIN_WIDTH, PARTICLE_MAX_WIDTH);
+		g_particles[i].size_y = rnd(PARTICLE_MIN_HEIGHT, PARTICLE_MAX_HEIGHT);
+		g_particles[i].pos_x = rnd(0, g_fb_width-g_particles[i].size_x-1);
+		g_particles[i].pos_y = rnd(0, g_fb_height-g_particles[i].size_y-1);
+		g_particles[i].dx = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
+		g_particles[i].dy = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
 
-		if (buttonsUp & WPAD_BUTTON_A ) {
-			printf("Button A released.\n");
-		}
+		if(rand() % 1) g_particles[i].dx = -g_particles[i].dx;
+		if(rand() % 1) g_particles[i].dy = -g_particles[i].dy;
 
-		/*if (PAD_StickY(WPAD_CHAN_0) > 18) {
-			printf("Joystick moved up.\n");
-		}
-
-		if (PAD_StickY(WPAD_CHAN_0) < -18) {
-			printf("Joystick moved down.\n");
-		}*/
-
-		VIDEO_ClearFrameBuffer(vmode, xfb, COLOR_BLACK);
-
-		// Console output
-		//printInfo();
-
-		// Draw stuff
-		DrawBox((g_fb_width>>1)-10, (g_fb_height>>1)-10,
-				(g_fb_width>>1)+10, (g_fb_height>>1)+10, COLOR_WHITE);
-		DrawBox(1, 1, g_fb_width-1, g_fb_height-1, COLOR_WHITE);
-		DrawParticle(g_fb_width>>1, g_fb_height>>1, 1, 1, COLOR_WHITE);
-
-		updateParticles();
-
-		// Wait for the next frame
-		VIDEO_WaitVSync();
+		// Determine frequency of collision sound depending on particle size
+		// (linear interpolation)
+		size = g_particles[i].size_x*g_particles[i].size_y;
+		g_particles[i].freq = PARTICLE_MIN_FREQ +
+							 (PARTICLE_MAX_FREQ - PARTICLE_MIN_FREQ) /
+							 (PARTICLE_MAX_SIZE - PARTICLE_MIN_SIZE) *
+							 (PARTICLE_MAX_SIZE - size);
 	}
+}
 
-	return 0;
+void updateParticles() {
+	int i;
+	for(i=0; i<NUM_PARTICLES; i++) {
+		// Apply changes in movement
+		g_particles[i].pos_x += g_particles[i].dx;
+		g_particles[i].pos_y += g_particles[i].dy;
+
+		// Check for collisions with screen boundaries
+		if(g_particles[i].pos_x < 0 || g_particles[i].pos_x > (g_fb_width-g_particles[i].size_x)) {
+			g_particles[i].dx = -g_particles[i].dx;
+			g_particles[i].pos_x = (g_particles[i].pos_x<0)?0:g_fb_width-g_particles[i].size_x;
+			voice=ASND_GetFirstUnusedVoice();
+			ASND_SetVoice(voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
+						 (u8 *)sound_pcm, sound_pcm_size, 255, 255, NULL);
+		}
+
+		if(g_particles[i].pos_y < 0 || g_particles[i].pos_y > (g_fb_height-g_particles[i].size_y)) {
+			g_particles[i].dy = -g_particles[i].dy;
+			g_particles[i].pos_y = (g_particles[i].pos_y<0)?0:g_fb_height-g_particles[i].size_y;
+			voice=ASND_GetFirstUnusedVoice();
+			ASND_SetVoice(voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
+						 (u8 *)sound_pcm, sound_pcm_size, 255, 255, NULL);
+		}
+
+		// Display updated particle
+		drawParticle(g_particles[i].pos_x,  g_particles[i].pos_y,
+					 g_particles[i].pos_x + g_particles[i].size_x - 1,
+					 g_particles[i].pos_y + g_particles[i].size_y - 1,
+					 COLOR_WHITE);
+	}
+}
+
+void printInfo() {
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 1
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;1H");
+
+	printf("Hello World!\n\n");
+
+	printf("\taa:        %d\n", vmode->aa);
+	printf("\tfbWidtht:  %d\n", vmode->fbWidth);
+	printf("\tefbHeight: %d\n", vmode->efbHeight);
+	printf("\txfbHeight: %d\n", vmode->xfbHeight);
+	printf("\txfbMode:   %d\n", vmode->xfbMode);
+	printf("\tviWidth:   %d\n", vmode->viWidth);
+	printf("\tviHeight:  %d\n", vmode->viHeight);
+	printf("\tviTVMode:  %d\n", vmode->viTVMode);
+	printf("\tviXOrigin: %d\n", vmode->viXOrigin);
+	printf("\tviYOrigin: %d\n", vmode->viYOrigin);
 }
 
 void init() {
@@ -121,25 +222,25 @@ void init() {
 	// Obtain the preferred video mode from the system
 	// This will correspond to the settings in the Wii menu
 	vmode = VIDEO_GetPreferredMode(NULL);
-	g_fb_width = vmode->fbWidth;
-	g_fb_height = vmode->xfbHeight;
 
-//	// widescreen fix
-//	if(CONF_GetAspectRatio() == CONF_ASPECT_16_9)
-//		vmode->viWidth = VI_MAX_WIDTH_PAL;
+	// widescreen fix
+	if(CONF_GetAspectRatio() == CONF_ASPECT_16_9)
+		vmode->viWidth = VI_MAX_WIDTH_PAL;
 
 	// Allocate memory for the display in the uncached region
 	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(vmode));
+	g_fb_width = vmode->fbWidth;
+	g_fb_height = vmode->xfbHeight;
 
 	// Initialise the console, required for printf
 	console_init(xfb,20,20,g_fb_width,g_fb_height,g_fb_width*VI_DISPLAY_PIX_SZ);
-	
+
 	// Set up the video registers with the chosen mode
 	VIDEO_Configure(vmode);
-	
+
 	// Tell the video hardware where our display memory is
 	VIDEO_SetNextFramebuffer(xfb);
-	
+
 	// Make the display visible
 	VIDEO_SetBlack(FALSE);
 
@@ -178,108 +279,67 @@ void init() {
 
 }
 
-void DrawHLine (int x1, int x2, int y, int color) {
-    int i;
-    y = 320 * y;
-    x1 >>= 1;
-    x2 >>= 1;
-    for (i = x1; i <= x2; i++) {
-        u32 *tmpfb = xfb;
-        tmpfb[y+i] = color;
-    }
-}
+/******************************************************************************
+ * Main method
+ *****************************************************************************/
+int main(int argc, char **argv) {
 
-void DrawVLine (int x, int y1, int y2, int color) {
-    int i;
-    x >>= 1;
-    for (i = y1; i <= y2; i++) {
-        u32 *tmpfb = xfb;
-        tmpfb[x + ((640 * i) >> 1)] = color;
-    }
-}
+	init();
 
-void DrawBox (int x1, int y1, int x2, int y2, int color) {
-    DrawHLine (x1, x2, y1, color);
-    DrawHLine (x1, x2, y2, color);
-    DrawVLine (x1, y1, y2, color);
-    DrawVLine (x2, y1, y2, color);
-}
+	while(1) {
+//		// Call WPAD_ScanPads each loop, this reads the latest controller states
+//		WPAD_ScanPads();
+//
+//		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+//		// this is a "one shot" state which will not fire again until the button
+//		// has been released
+//		u32 pressed = WPAD_ButtonsDown(WPAD_CHAN_0);
+//
+//		// We return to the launcher application via exit
+//		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+//
+//		if( pressed & WPAD_BUTTON_A ) {
+//			printf("Button A pressed.\n");
+//		}
+//
+//		u32 buttonsHeld = WPAD_ButtonsHeld(WPAD_CHAN_0);
+//
+//		if (buttonsHeld & WPAD_BUTTON_A ) {
+//			printf("Button A is being held down.\n");
+//		}
+//
+//		u16 buttonsUp = WPAD_ButtonsUp(WPAD_CHAN_0);
+//
+//		if (buttonsUp & WPAD_BUTTON_A ) {
+//			printf("Button A released.\n");
+//		}
+//
+//		/*if (PAD_StickY(WPAD_CHAN_0) > 18) {
+//			printf("Joystick moved up.\n");
+//		}
+//
+//		if (PAD_StickY(WPAD_CHAN_0) < -18) {
+//			printf("Joystick moved down.\n");
+//		}*/
+//
+		VIDEO_ClearFrameBuffer(vmode, xfb, COLOR_BLACK);
 
-void DrawParticle(int x, int y, int width, int height, int color) {
-	int i;
-	for(i=0; i<height; i++)
-		DrawHLine(x, x+width, y+i, color);
-}
+		// Console output
+		//printInfo();
 
-void initParticles() {
-	srand(time(NULL));
-	int i;
-	for(i=0; i<NUM_PARTICLES; i++) {
-		// Create particle with random size, position and speed
-		g_particles[i].size_x = rand() % (PARTICLE_MAX_WIDTH-PARTICLE_MIN_WIDTH+1) + PARTICLE_MIN_WIDTH;
-		g_particles[i].size_y = rand() % (PARTICLE_MAX_HEIGHT-PARTICLE_MIN_HEIGHT+1) + PARTICLE_MIN_HEIGHT;
-		g_particles[i].pos_x = rand() % (g_fb_width-g_particles[i].size_x);
-		g_particles[i].pos_y = rand() % (g_fb_height-g_particles[i].size_y);
-		g_particles[i].dx = (rand() % PARTICLE_MAX_SPEED-PARTICLE_MIN_SPEED+1) + PARTICLE_MIN_SPEED;
-		g_particles[i].dy = (rand() % PARTICLE_MAX_SPEED-PARTICLE_MIN_SPEED+1) + PARTICLE_MIN_SPEED;
-
-		if(rand() % 1) g_particles[i].dx = -g_particles[i].dx;
-		if(rand() % 1) g_particles[i].dy = -g_particles[i].dy;
-	}
-}
-
-void updateParticles() {
-
-	int i, size, freq, freq_min, freq_max;
-	for(i=0; i<NUM_PARTICLES; i++) {
-
-		// Apply changes in movement
-		g_particles[i].pos_x += g_particles[i].dx;
-		g_particles[i].pos_y += g_particles[i].dy;
-
-		size = g_particles[i].size_x*g_particles[i].size_y;
-		freq_max = VOICE_FREQ48KHZ;
-		freq_min = VOICE_FREQ48KHZ/2;
-		freq = freq_min + (freq_max - freq_min)/(PARTICLE_MAX_SIZE-PARTICLE_MIN_SIZE) * (PARTICLE_MAX_SIZE-size);
-
-		// Check for collisions with screen boundaries
-		if(g_particles[i].pos_x < 1 || g_particles[i].pos_x >= (g_fb_width-g_particles[i].size_x)) {
-			g_particles[i].dx = -g_particles[i].dx;
-			voice=ASND_GetFirstUnusedVoice();
-			ASND_SetVoice(voice, VOICE_MONO_16BIT,freq, 0, (u8 *)sound_pcm, sound_pcm_size, 255, 255, NULL);
-		}
-
-		if(g_particles[i].pos_y < 1 || g_particles[i].pos_y >= (g_fb_height-g_particles[i].size_y)) {
-			g_particles[i].dy = -g_particles[i].dy;
-			voice=ASND_GetFirstUnusedVoice();
-			ASND_SetVoice(voice, VOICE_MONO_16BIT,freq, 0, (u8 *)sound_pcm, sound_pcm_size, 255, 255, NULL);
-		}
-
-		// Display particle
-		DrawParticle(g_particles[i].pos_x, g_particles[i].pos_y,
-					 g_particles[i].size_x, g_particles[i].size_y,
+		// Draw stuff
+		drawBox((g_fb_width>>1)-10, (g_fb_height>>1)-10,
+				(g_fb_width>>1)+10, (g_fb_height>>1)+10, COLOR_WHITE);
+		drawBox(1, 1, g_fb_width-1, g_fb_height-1, COLOR_WHITE);
+		drawParticle(g_fb_width>>1, g_fb_height>>1,
+				     g_fb_width>>1, g_fb_height>>1,
 					 COLOR_WHITE);
+
+		updateParticles();
+
+		// Wait for the next frame
+		VIDEO_WaitVSync();
 	}
-}
 
-void printInfo() {
-
-	// The console understands VT terminal escape codes
-	// This positions the cursor on row 2, column 1
-	// we can use variables for this with format codes too
-	// e.g. printf ("\x1b[%d;%dH", row, column );
-	printf("\x1b[2;1H");
-
-	printf("Hello World!\n\n");
-
-	printf("\taa:        %d\n", vmode->aa);
-	printf("\tfbWidtht:  %d\n", vmode->fbWidth);
-	printf("\tefbHeight: %d\n", vmode->efbHeight);
-	printf("\txfbHeight: %d\n", vmode->xfbHeight);
-	printf("\txfbMode:   %d\n", vmode->xfbMode);
-	printf("\tviHeight:  %d\n", vmode->viHeight);
-	printf("\tviWidth:   %d\n", vmode->viWidth);
-	printf("\tviTVMode:  %d\n", vmode->viTVMode);
-	printf("\tviXOrigin: %d\n", vmode->viXOrigin);
-	printf("\tviYOrigin: %d\n", vmode->viYOrigin);
+	return 0;
 }
