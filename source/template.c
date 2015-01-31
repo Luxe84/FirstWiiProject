3470a138e484ab81815dd46a461908389bbee0e3
Andreas Luxenburger <luxenburger.andreas@gmail.com>
2015-01-31 11:42:18 +0100

    - Fixed display issues by adding margins - Foundations for a basic pong game (2 players)

--- source/template.c
+++ source/template.c
@@ -11,13 +11,20 @@
 #include "sound_pcm.h"
 #include "bg_music_ogg.h"
 
-#define NUM_PARTICLES 10
+#define NUM_PLAYERS 2
+#define NUM_PARTICLES 1
 #define PARTICLE_MAX_WIDTH 20
 #define PARTICLE_MAX_HEIGHT 20
 #define PARTICLE_MIN_WIDTH 2
 #define PARTICLE_MIN_HEIGHT 2
 #define PARTICLE_MAX_SPEED 10
 #define PARTICLE_MIN_SPEED 1
+#define MARGIN_TOP_PERCENT 5
+#define MARGIN_BOTTOM_PERCENT 10
+#define MARGIN_LEFT_PERCENT 5
+#define MARGIN_RIGHT_PERCENT 5
+#define TOKEN_SIZE_X 5
+#define TOKEN_SIZE_Y 50
 
 const int PARTICLE_MAX_FREQ  = VOICE_FREQ48KHZ;
 const int PARTICLE_MIN_FREQ = VOICE_FREQ48KHZ>>1;
@@ -30,19 +37,31 @@ int g_fbi=0;						// index of current framebuffer
 static GXRModeObj *g_vmode = NULL;	// ref. to render mode object
 int g_fb_height, g_fb_width;		// dimensions of external fb
 s32 g_voice;						// voice handle
-WPADData *g_wpd = NULL;				// for handling controller input
+WPADData *g_wpd[NUM_PLAYERS];		// for handling controller input
 s32 g_shutDownType = -1;			// flag for callback functions
 int evctr = 0;						// event counter
 int g_simulate = 1;					// should the particle simulation run?
+
+int g_border_t;						// upper display boundary
+int g_border_b;						// lower display boundary
+int g_border_l;						// left display boundary
+int g_border_r;						// right display boundary
+int g_vi_width;						// dimension of the display in x-dir
+int g_vi_height;					// dimension of the display in y-dir
 
 typedef struct {
 	int pos_x, pos_y;   // screen coordinates
 	int size_x, size_y; // dimensions
 	int dx, dy;         // speed values
-	int freq;		    // freequency of collision sound
+	int freq;		    // frequency of collision sound
 }Particle;
 
+// Particles moving around
 Particle g_particles[NUM_PARTICLES];
+
+// Player's token for blocking particles
+Particle g_player_token[NUM_PLAYERS];
+int g_token_colors[4] = {COLOR_RED, COLOR_GREEN, COLOR_WHITE, COLOR_BLUE};
 
 // Callback functions
 
@@ -235,90 +254,30 @@ void drawEllipse(int xm, int ym, int a, int b, int color)
    }
 }
 
-void displayIR() {
+void displayIR(int chan) {
 	int i;
 	float theta;
 
 	// IR dots
-	for(i=0; i<4; i++) {
-		if(g_wpd->ir.dot[i].visible) {
-			drawdot(1024, 768, g_wpd->ir.dot[i].rx, g_wpd->ir.dot[i].ry, COLOR_YELLOW);
+	for(i=0; i<NUM_PLAYERS; i++) {
+		if(g_wpd[chan]->ir.dot[i].visible) {
+			drawdot(1024, 768, g_wpd[chan]->ir.dot[i].rx, g_wpd[chan]->ir.dot[i].ry,
+					COLOR_YELLOW);
 		}
 	}
 	// Sensor bar as it is seen by the wiimote
-	if(g_wpd->ir.raw_valid) {
+	if(g_wpd[chan]->ir.raw_valid) {
 		for(i=0; i<2; i++) {
-			drawdot(4, 4, g_wpd->ir.sensorbar.rot_dots[i].x+2, g_wpd->ir.sensorbar.rot_dots[i].y+2, COLOR_GREEN);
+			drawdot(4, 4,
+					g_wpd[chan]->ir.sensorbar.rot_dots[i].x+2, g_wpd[chan]->ir.sensorbar.rot_dots[i].y+2,
+					COLOR_GREEN);
 		}
 	}
 	// Cursor
-	if(g_wpd->ir.valid) {
-		theta = g_wpd->ir.angle / 180.f * M_PI;
-		drawdot(g_fb_width, g_fb_height, g_wpd->ir.x, g_wpd->ir.y, COLOR_RED);
-		drawdot(g_fb_width, g_fb_height, g_wpd->ir.x + 10*sinf(theta), g_wpd->ir.y - 10*cosf(theta), COLOR_BLUE);
-	}
-}
-
-// Init and update routines
-
-int rnd(int a, int b) {
-	return rand() % (b-a+1) + a;
-}
-
-void initParticles() {
-	srand(time(NULL));
-	int i, size;
-	for(i=0; i<NUM_PARTICLES; i++) {
-		// Create particle with random size, position and speed
-		g_particles[i].size_x = rnd(PARTICLE_MIN_WIDTH, PARTICLE_MAX_WIDTH);
-		g_particles[i].size_y = rnd(PARTICLE_MIN_HEIGHT, PARTICLE_MAX_HEIGHT);
-		g_particles[i].pos_x = rnd(0, g_fb_width-g_particles[i].size_x-1);
-		g_particles[i].pos_y = rnd(0, g_fb_height-g_particles[i].size_y-1);
-		g_particles[i].dx = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
-		g_particles[i].dy = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
-
-		if(rand() % 1) g_particles[i].dx = -g_particles[i].dx;
-		if(rand() % 1) g_particles[i].dy = -g_particles[i].dy;
-
-		// Determine frequency of collision sound depending on particle size
-		// (linear interpolation)
-		size = g_particles[i].size_x*g_particles[i].size_y;
-		g_particles[i].freq = PARTICLE_MIN_FREQ +
-							 (PARTICLE_MAX_FREQ - PARTICLE_MIN_FREQ) /
-							 (PARTICLE_MAX_SIZE - PARTICLE_MIN_SIZE) *
-							 (PARTICLE_MAX_SIZE - size);
-	}
-}
-
-void updateParticles() {
-	int i;
-	for(i=0; i<NUM_PARTICLES; i++) {
-		// Apply changes in movement
-		g_particles[i].pos_x += g_particles[i].dx;
-		g_particles[i].pos_y += g_particles[i].dy;
-
-		// Check for collisions with screen boundaries
-		if(g_particles[i].pos_x < 0 || g_particles[i].pos_x > (g_fb_width-g_particles[i].size_x)) {
-			g_particles[i].dx = -g_particles[i].dx;
-			g_particles[i].pos_x = (g_particles[i].pos_x<0)?0:g_fb_width-g_particles[i].size_x;
-			g_voice=ASND_GetFirstUnusedVoice();
-			ASND_SetVoice(g_voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
-						 (u8 *)sound_pcm, sound_pcm_size, 63, 63, NULL);
-		}
-
-		if(g_particles[i].pos_y < 0 || g_particles[i].pos_y > (g_fb_height-g_particles[i].size_y)) {
-			g_particles[i].dy = -g_particles[i].dy;
-			g_particles[i].pos_y = (g_particles[i].pos_y<0)?0:g_fb_height-g_particles[i].size_y;
-			g_voice=ASND_GetFirstUnusedVoice();
-			ASND_SetVoice(g_voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
-						 (u8 *)sound_pcm, sound_pcm_size, 63, 63, NULL);
-		}
-
-		// Display updated particle
-		drawParticle(g_particles[i].pos_x,  g_particles[i].pos_y,
-					 g_particles[i].pos_x + g_particles[i].size_x - 1,
-					 g_particles[i].pos_y + g_particles[i].size_y - 1,
-					 COLOR_WHITE);
+	if(g_wpd[chan]->ir.valid) {
+		theta = g_wpd[chan]->ir.angle / 180.f * M_PI;
+		drawdot(g_fb_width, g_fb_height, g_wpd[chan]->ir.x, g_wpd[chan]->ir.y, COLOR_RED);
+		drawdot(g_fb_width, g_fb_height, g_wpd[chan]->ir.x + 10*sinf(theta), g_wpd[chan]->ir.y - 10*cosf(theta), COLOR_BLUE);
 	}
 }
 
@@ -347,61 +306,159 @@ void printVideoInfo() {
 	}
 }
 
-void printWiimoteinfo() {
-	if(g_wpd!=NULL) {
+void printWiimoteinfo(int chan) {
+	if(g_wpd[chan]!=NULL&&chan>=0&&chan<4) {
 		int i;
 		printf("\n");
 		printf(" Event count: %d\n",evctr);
-		printf(" Battery Level: %d\n", g_wpd->battery_level);
-		printf(" Data->Err: %d\n",g_wpd->err);
+		printf(" Battery Level: %d\n", g_wpd[chan]->battery_level);
+		printf(" Data->Err: %d\n",g_wpd[chan]->err);
 		printf(" IR Dots:\n");
 		for(i=0; i<4; i++) {
-			if(g_wpd->ir.dot[i].visible) {
-				printf(" %4d, %3d\n", g_wpd->ir.dot[i].rx, g_wpd->ir.dot[i].ry);
+			if(g_wpd[chan]->ir.dot[i].visible) {
+				printf(" %4d, %3d\n", g_wpd[chan]->ir.dot[i].rx, g_wpd[chan]->ir.dot[i].ry);
 			}
 			else {
 				printf(" None\n");
 			}
 		}
-		if(g_wpd->ir.valid) {
-			printf(" Cursor: %.02f, %.02f\n", g_wpd->ir.x, g_wpd->ir.y);
-			printf(" Angle: %.02f deg\n", g_wpd->ir.angle);
+		if(g_wpd[chan]->ir.valid) {
+			printf(" Cursor: %.02f, %.02f\n", g_wpd[chan]->ir.x, g_wpd[chan]->ir.y);
+			printf(" Angle: %.02f deg\n", g_wpd[chan]->ir.angle);
 		}
 		else {
 			printf(" No Cursor\n\n");
 		}
-		if(g_wpd->ir.raw_valid) {
-			printf(" Distance: %.02fm\n", g_wpd->ir.z);
-			printf(" Yaw: %.02f deg\n", g_wpd->orient.yaw);
+		if(g_wpd[chan]->ir.raw_valid) {
+			printf(" Distance: %.02fm\n", g_wpd[chan]->ir.z);
+			printf(" Yaw: %.02f deg\n", g_wpd[chan]->orient.yaw);
 		} else {
 			printf("\n\n");
 		}
 		printf(" Accel:\n");
-		printf(" XYZ: %3d,%3d,%3d\n",g_wpd->accel.x,g_wpd->accel.y,g_wpd->accel.z);
-		printf(" Pitch: %.02f\n",g_wpd->orient.pitch);
-		printf(" Roll: %.02f\n",g_wpd->orient.roll);
+		printf(" XYZ: %3d,%3d,%3d\n",g_wpd[chan]->accel.x,g_wpd[chan]->accel.y,g_wpd[chan]->accel.z);
+		printf(" Pitch: %.02f\n",g_wpd[chan]->orient.pitch);
+		printf(" Roll: %.02f\n",g_wpd[chan]->orient.roll);
 		printf(" Buttons down:\n ");
-		if(g_wpd->btns_h & WPAD_BUTTON_A) printf("A ");
-		if(g_wpd->btns_h & WPAD_BUTTON_B) printf("B ");
-		if(g_wpd->btns_h & WPAD_BUTTON_1) printf("1 ");
-		if(g_wpd->btns_h & WPAD_BUTTON_2) printf("2 ");
-		if(g_wpd->btns_h & WPAD_BUTTON_MINUS) printf("MINUS ");
-		if(g_wpd->btns_h & WPAD_BUTTON_HOME) printf("HOME ");
-		if(g_wpd->btns_h & WPAD_BUTTON_PLUS) printf("PLUS ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_A) printf("A ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_B) printf("B ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_1) printf("1 ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_2) printf("2 ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_MINUS) printf("MINUS ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_HOME) printf("HOME ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_PLUS) printf("PLUS ");
 		printf("\n ");
-		if(g_wpd->btns_h & WPAD_BUTTON_LEFT) printf("LEFT ");
-		if(g_wpd->btns_h & WPAD_BUTTON_RIGHT) printf("RIGHT ");
-		if(g_wpd->btns_h & WPAD_BUTTON_UP) printf("UP ");
-		if(g_wpd->btns_h & WPAD_BUTTON_DOWN) printf("DOWN ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_LEFT) printf("LEFT ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_RIGHT) printf("RIGHT ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_UP) printf("UP ");
+		if(g_wpd[chan]->btns_h & WPAD_BUTTON_DOWN) printf("DOWN ");
 		printf("\n");
 	}
 }
 
-void init() {
 
-	/*************************************************************************
-	 * VIDEO                                                                 *
-	 *************************************************************************/
+// Init and update routines
+
+int rnd(int a, int b) {
+	return rand() % (b-a+1) + a;
+}
+
+void initToken() {
+	int i;
+	for(i=0; i<NUM_PLAYERS; i++)
+	{
+		g_player_token[i].dx = 0;
+		g_player_token[i].dy = 0;
+		g_player_token[i].size_x = TOKEN_SIZE_X;
+		g_player_token[i].size_y = TOKEN_SIZE_Y;
+		g_player_token[i].pos_x = (i%2==0)?(g_border_l+2):(g_border_r-g_player_token[i].size_x);
+		g_player_token[i].pos_y = g_border_t+((g_vi_height-g_player_token[i].size_y)>>1);
+	}
+}
+
+void updateToken() {
+	int i, pos_y;
+	for(i=0; i<NUM_PLAYERS; i++)
+	{
+		// TODO Fit wiimote motion to token
+		if(g_wpd[i]->ir.valid)
+		{
+			pos_y = g_wpd[i]->ir.y - (g_player_token[i].size_y>>1);
+//			g_player_token[i].pos_y = (pos_y<g_border_t)?g_border_t:pos_y;
+//			if(pos_y>g_border_b-g_player_token[i].size_y)
+//				g_player_token[i].pos_y = g_border_b - g_player_token[i].size_y;
+		}
+
+		// Draw token
+		drawParticle(g_player_token[i].pos_x,  g_player_token[i].pos_y,
+					 g_player_token[i].pos_x + g_player_token[i].size_x - 1,
+					 g_player_token[i].pos_y + g_player_token[i].size_y - 1,
+					 g_token_colors[i]);
+	}
+}
+
+void initParticles() {
+	srand(time(NULL));
+	int i, size;
+	for(i=0; i<NUM_PARTICLES; i++) {
+		// Create particle with random size, position and speed
+		g_particles[i].size_x = rnd(PARTICLE_MIN_WIDTH, PARTICLE_MAX_WIDTH);
+		g_particles[i].size_y = rnd(PARTICLE_MIN_HEIGHT, PARTICLE_MAX_HEIGHT);
+		g_particles[i].pos_x = rnd(g_border_l, g_border_r-g_particles[i].size_x-1);
+		g_particles[i].pos_y = rnd(g_border_t, g_border_b-g_particles[i].size_y-1);
+		g_particles[i].dx = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
+		g_particles[i].dy = rnd(PARTICLE_MIN_SPEED, PARTICLE_MAX_SPEED);
+
+		if(rand() % 1) g_particles[i].dx = -g_particles[i].dx;
+		if(rand() % 1) g_particles[i].dy = -g_particles[i].dy;
+
+		// Determine frequency of collision sound depending on particle size
+		// (linear interpolation)
+		size = g_particles[i].size_x*g_particles[i].size_y;
+		g_particles[i].freq = PARTICLE_MIN_FREQ +
+							 (PARTICLE_MAX_FREQ - PARTICLE_MIN_FREQ) /
+							 (PARTICLE_MAX_SIZE - PARTICLE_MIN_SIZE) *
+							 (PARTICLE_MAX_SIZE - size);
+	}
+}
+
+void updateParticles() {
+	int i;
+	for(i=0; i<NUM_PARTICLES; i++) {
+		// Apply changes in movement
+		g_particles[i].pos_x += g_particles[i].dx;
+		g_particles[i].pos_y += g_particles[i].dy;
+
+		// Check for collisions with screen boundaries
+		if(g_particles[i].pos_x < g_border_l || g_particles[i].pos_x > (g_border_r-g_particles[i].size_x)) {
+			g_particles[i].dx = -g_particles[i].dx;
+			g_particles[i].pos_x = (g_particles[i].pos_x<g_border_l)?g_border_l:g_border_r-g_particles[i].size_x;
+			g_voice=ASND_GetFirstUnusedVoice();
+			ASND_SetVoice(g_voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
+						 (u8 *)sound_pcm, sound_pcm_size, 63, 63, NULL);
+		}
+
+		if(g_particles[i].pos_y < g_border_t || g_particles[i].pos_y > (g_border_b-g_particles[i].size_y)) {
+			g_particles[i].dy = -g_particles[i].dy;
+			g_particles[i].pos_y = (g_particles[i].pos_y<g_border_t)?g_border_t:g_border_b-g_particles[i].size_y;
+			g_voice=ASND_GetFirstUnusedVoice();
+			ASND_SetVoice(g_voice, VOICE_MONO_16BIT, g_particles[i].freq, 0,
+						 (u8 *)sound_pcm, sound_pcm_size, 63, 63, NULL);
+		}
+
+		// Display updated particle
+		drawParticle(g_particles[i].pos_x,  g_particles[i].pos_y,
+					 g_particles[i].pos_x + g_particles[i].size_x - 1,
+					 g_particles[i].pos_y + g_particles[i].size_y - 1,
+					 COLOR_WHITE);
+	}
+}
+
+
+/*****************************************************************************
+ * Initialization of the video system                                        *                                  *
+ *****************************************************************************/
+void initVideo() {
 	// Initialise the video system
 	VIDEO_Init();
 
@@ -435,43 +492,76 @@ void init() {
 	VIDEO_WaitVSync();
 	if(g_vmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
-	/*************************************************************************
-	 * AUDIO                                                                 *
-	 *************************************************************************/
+	// Setup display region
+	g_border_t = (int)((float)g_fb_height * MARGIN_TOP_PERCENT) / 100.f;
+	g_border_b = (int)((float)g_fb_height *
+				 (1.f - MARGIN_BOTTOM_PERCENT/100.f));
+	g_border_l = (int)((float)g_fb_width * MARGIN_LEFT_PERCENT) / 100.f;
+	g_border_r = (int)((float)g_fb_width * (1.f - MARGIN_RIGHT_PERCENT/100.f));
+	g_vi_width = g_border_r - g_border_l;
+	g_vi_height = g_border_b - g_border_t;
+}
+
+/*****************************************************************************
+ * Initialization of the Audio system                                        *                                  *
+ *****************************************************************************/
+void initAudio() {
 	// Initialise the audio subsysten
 	AUDIO_Init(NULL);
 	ASND_Init(NULL);
 	ASND_Pause(0);
-	PlayOgg(bg_music_ogg, bg_music_ogg_size, 0, OGG_INFINITE_TIME);
 
-	/*************************************************************************
-	 * CONTROLS                                                              *
-	 *************************************************************************/
-	// This function initialises the attached controllers
+	// Start background music
+	PlayOgg(bg_music_ogg, bg_music_ogg_size, 0, OGG_INFINITE_TIME);
+}
+
+/*****************************************************************************
+ * Initialization of the Control system                                      *                                  *
+ *****************************************************************************/
+void initControls() {
+	int i;
+	// Initialize the attached controllers
 	WPAD_Init();
 
-	//	Configure infrared system of the Wii remote
-	WPAD_SetVRes(WPAD_CHAN_0, g_fb_width, g_fb_height);
-	WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
+	for(i=0; i<NUM_PLAYERS;i++)
+	{
+		//	Configure infrared system of the Wii remote
+		WPAD_SetVRes(i, g_fb_width, g_fb_height);
+		WPAD_SetDataFormat(i, WPAD_FMT_BTNS_ACC_IR);
 
-	// Install callbacks
-	SYS_SetResetCallback(cb_WiiResetButtonPressed);
-	SYS_SetPowerCallback(cb_WiiPowerButtonPressed);
-	WPAD_SetPowerButtonCallback(cb_WiimotePowerButtonPressed);
+		// Install callbacks
+		SYS_SetResetCallback(cb_WiiResetButtonPressed);
+		SYS_SetPowerCallback(cb_WiiPowerButtonPressed);
+		WPAD_SetPowerButtonCallback(cb_WiimotePowerButtonPressed);
+	}
+}
+
+/*****************************************************************************
+ * Method for initialization                                                 *                                  *
+ *****************************************************************************/
+void init() {
+
+	/*************************************************************************
+	 * VIDEO, AUDIO, CONTROLS                                                *
+	 *************************************************************************/
+	initVideo();
+	initAudio();
+	initControls();
 
 	/*************************************************************************
 	 * GAME RELATED STUFF                                                    *
 	 *************************************************************************/
 	// Setup particle system
 	initParticles();
-
+	initToken();
 }
 
 /******************************************************************************
  * Main method
  *****************************************************************************/
 int main(int argc, char **argv) {
-	int ret;
+	int ret[NUM_PLAYERS];
+	int i;
 	u32 type;
 
 	// Initialization
@@ -480,51 +570,56 @@ int main(int argc, char **argv) {
 	// Game loop
 	while(g_shutDownType==-1) {
 		// Setup console, required for printf
-		console_init(g_xfb[g_fbi], 0, 0, g_fb_width, g_fb_height,
+		console_init(g_xfb[g_fbi], 0, 0,
+					 g_fb_width, g_fb_height,
 					 g_fb_width * VI_DISPLAY_PIX_SZ);
 		VIDEO_ClearFrameBuffer(g_vmode, g_xfb[g_fbi], COLOR_BLACK);
 		//printVideoInfo();
 
 		// Check status of the Wiimote
 		WPAD_ReadPending(WPAD_CHAN_ALL, cb_WiimoteEventFired);
-		ret = WPAD_Probe(WPAD_CHAN_0, &type);
-		switch(ret) {
-			case WPAD_ERR_NO_CONTROLLER:
-				printf(" Wiimote not connected\n");
-				break;
-			case WPAD_ERR_NOT_READY:
-				printf(" Wiimote not ready\n");
-				break;
-			case WPAD_ERR_NONE:
-				printf(" Wiimote ready\n");
-				break;
-			default:
-				printf(" Unknown Wiimote status %d\n", ret);
-		}
-
-		// If no error occured, process retrieved controller input
-		if(ret == WPAD_ERR_NONE)
+		for(i=0; i<NUM_PLAYERS; i++)
 		{
-			g_wpd = WPAD_Data(WPAD_CHAN_0);
-			printWiimoteinfo();
-			displayIR();
+			ret[i] = WPAD_Probe(i, &type);
+			switch(ret[i]) {
+				case WPAD_ERR_NO_CONTROLLER:
+					printf(" Wiimote %d not connected\n", i);
+					break;
+				case WPAD_ERR_NOT_READY:
+					printf(" Wiimote %d not ready\n", i);
+					break;
+				case WPAD_ERR_NONE:
+					printf(" Wiimote %d ready\n", i);
+					break;
+				default:
+					printf(" Unknown status of Wiimote %d: %d\n", i, ret[i]);
+			}
+
+			// If no error occured, process retrieved controller input
+			if(ret[i] == WPAD_ERR_NONE)
+			{
+				g_wpd[i] = WPAD_Data(i);
+				//printWiimoteinfo(i);
+				displayIR(i);
+			}
 		}
 
 		// Display background
-		drawBox((g_fb_width>>1)-10, (g_fb_height>>1)-10,
-				(g_fb_width>>1)+10, (g_fb_height>>1)+10, COLOR_WHITE);
-		drawBox(0, 0, g_fb_width-1, g_fb_height-1, COLOR_WHITE);
-		drawVLine(g_fb_width>>1, 0, g_fb_height-1, COLOR_WHITE);
+//		drawBox(g_border_l + (g_vi_width>>1)-10, g_border_t + (g_vi_height>>1)-10,
+//				g_border_l + (g_vi_width>>1)+10, g_border_t + (g_vi_height>>1)+10,
+//				COLOR_WHITE);
+		drawEllipse(g_border_l + (g_vi_width>>1), g_border_t + (g_vi_height>>1),
+					10, 10, COLOR_WHITE);
+		drawBox(g_border_l, g_border_t, g_border_r, g_border_b, COLOR_WHITE);
+		drawVLine(g_border_l + (g_vi_width>>1), g_border_t, g_border_b, COLOR_WHITE);
 
-
-		drawLine(100, 100, 200, 300, COLOR_WHITE);
-		drawEllipse(200, 200, 20, 10, COLOR_WHITE);
-		drawCircle(g_fb_width>>1, g_fb_height>>1, 10, COLOR_WHITE);
-
+		//drawLine(100, 100, 200, 300, COLOR_WHITE);
 
 		// Update game engine and render changes
 		if(g_simulate)
 			updateParticles();
+
+		updateToken();
 
 		// Wait for the next frame and switch framebuffer
 		VIDEO_SetNextFramebuffer(g_xfb[g_fbi]);
